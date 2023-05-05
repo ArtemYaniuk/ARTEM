9186c1d1e32dc50e5cc0e6e71fb8656f1f6978ed
ArtemYaniuk <yaniukartem0309@gmail.com>
2023-05-05 15:41:50 +0200

    +

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import time
+
+WIDTH = 1640
+HEIGHT = 1080
+FPS = 144
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+jhjkhkhkhjhkknhjgjb
