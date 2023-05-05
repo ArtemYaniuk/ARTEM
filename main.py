c1b2137ee3f256f3f6fb51d6b74553cd83ddf7eb
ArtemYaniuk <yaniukartem0309@gmail.com>
2023-05-05 16:24:16 +0200

    +FPS

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import pygame
 import random
 import time
 
-WIDTH = 1640
-HEIGHT = 1080
+WIDTH = 1000
+HEIGHT = 700
 FPS = 144
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-jhjkhkhkhjhkknhjgjb
+pygame.display.set_caption("Танки 2D")
+clock = pygame.time.Clock()
+
+font = pygame.font.Font(None, 30) 
+font1 = pygame.font.Font(None, 20)
+running = True
+
+while running:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(font.render(str(round(clock.get_fps())), True, (255, 255, 150)), (10, 10))
+    screen.blit(font1.render(str("FPS"), True, (255, 255, 150)), (50, 10))
+    clock.tick(FPS)
+    pygame.display.flip()
