1b7b3dc5dbb6ecef2aba7ddc0108c1a2adb9706f
ArtemYaniuk <yaniukartem0309@gmail.com>
2023-05-14 00:34:18 +0200

    SNAKE

--- main.py
+++ main.py
@@ -1,27 +1,73 @@
 import pygame
-import random
-import time
+from random import randrange
 
-WIDTH = 1000
-HEIGHT = 700
-FPS = 144
+RES = 800
+SIZE = 50
+
+x, y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+dirs = {"W": True, "S": True, "A": True, "D": True, }
+score = 0
+lenght = 1
+snake = [(x, y)]
+dx, dy = 0, 0 
+fps = 5
+
 
 pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Танки 2D")
+sc = pygame.display.set_mode([RES, RES])
 clock = pygame.time.Clock()
+font_score = pygame.font.SysFont("Arial", 26, bold=True)
+fond_end = pygame.font.SysFont("Arial", 66, bold=True)
 
-font = pygame.font.Font(None, 30) 
-font1 = pygame.font.Font(None, 20)
-running = True
+while True:
+    sc.fill(pygame.Color("black"))
+    # малюем змейку, яблоко
+    [(pygame.draw.rect(sc, pygame.Color("green"), (i, j, SIZE - 2, SIZE - 2))) for i, j in snake]
+    pygame.draw.rect(sc, pygame.Color("red"), (*apple, SIZE, SIZE))
+    #показать щет
+    render_score = font_score.render(f"SCORE: {score}", 1, pygame.Color("orange"))
+    sc.blit(render_score, (5, 5))
+    #передвежение змейки(дальность)
+    x += dx * SIZE
+    y += dy * SIZE
+    snake.append((x, y))
+    snake = snake[-lenght:]
+    #поеданее яблока
+    if snake[-1] == apple:
+        apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
+        lenght += 1
+        fps += 1
+        score += 1
 
-while running:
-    screen.fill((0,0,0))
+    #Проигрш
+    if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
+        while True:
+            render_end = fond_end.render(f"GAME OVER", 1, pygame.Color("orange"))
+            sc.blit(render_end, (RES // 2 - 200, RES // 3))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+
+    pygame.display.flip()
+    clock.tick(fps)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    screen.blit(font.render(str(round(clock.get_fps())), True, (255, 255, 150)), (10, 10))
-    screen.blit(font1.render(str("FPS"), True, (255, 255, 150)), (50, 10))
-    clock.tick(FPS)
-    pygame.display.flip()
+            exit()
+
+    #контроль над змейкой
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] and dirs["W"]:
+        dx, dy = 0, -1
+        dirs = {"W": True, "S": False, "A": True, "D": True, }
+    if key[pygame.K_s] and dirs["S"]:
+        dx, dy = 0, 1
+        dirs = {"W": False, "S": True, "A": True, "D": True, }
+    if key[pygame.K_a] and dirs["A"]:
+        dx, dy = -1, 0
+        dirs = {"W": True, "S": True, "A": True, "D": False, }
+    if key[pygame.K_d] and dirs["D"]:
+        dx, dy = 1, 0
+        dirs = {"W": True, "S": True, "A": False, "D": True, }
